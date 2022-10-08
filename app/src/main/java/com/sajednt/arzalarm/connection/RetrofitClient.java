6cbd9b7a50b32914f0a7fb11977d128b5a1b6124
sajednt <sajednt@yahoo.com>
2022-10-08 09:53:52 +0330

    Update RetrofitClient.java

--- app/src/main/java/com/sajednt/arzalarm/connection/RetrofitClient.java
+++ app/src/main/java/com/sajednt/arzalarm/connection/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    public static final String API_URL = "https://arzalarm.com/";
+    public static final String API_URL = "---";
 
     private static RetrofitClient instance = null;
     GerritAPI gerritAPI;
